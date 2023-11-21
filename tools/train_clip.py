9a110538b9256d86b42e41a628e5f122ae0e6223
jianzhnie <jianzhnie@126.com>
2023-11-21 14:32:22 +0800

    update mmtransformers

--- tools/train_clip.py
+++ tools/train_clip.py
@@ -9,7 +9,7 @@ from mmengine.optim.scheduler import MultiStepLR
 from mmengine.runner import Runner
 from transformers import BertTokenizer
 
-sys.path.append('../')
+sys.path.append("../")
 from lmms.datasets.clip_dataset import Flikr8kDataset
 from lmms.models.clip.config import ClipConfig as cfg
 from lmms.models.clip.model import CLIPModel
@@ -19,25 +19,32 @@ def split_data(dataset: Flikr8kDataset, val_split: float):
     train_length = int((1 - val_split) * len(dataset))
     val_length = len(dataset) - train_length
     train_dataset, val_dataset = random_split(
-        dataset, lengths=[train_length, val_length])
+        dataset, lengths=[train_length, val_length]
+    )
     return train_dataset, val_dataset
 
 
 class MMClipModel(BaseModel):
-
     def __init__(self, model):
         super().__init__()
         self.model = model
 
-    def forward(self, input_ids, attention_mask, images, caption, mode='loss'):
+    def forward(
+        self,
+        input_ids,
+        attention_mask,
+        images,
+        caption,
+        mode="loss",
+    ):
         loss = self.model(
             input_ids,
             attention_mask,
             images,
             caption,
         )
-        if mode == 'loss':
-            return {'loss': loss}
+        if mode == "loss":
+            return {"loss": loss}
 
 
 def main() -> None:
@@ -45,8 +52,8 @@ def main() -> None:
     dataset = Flikr8kDataset(
         image_dir=cfg.image_path,
         tokenizer=tokenizer,
-        target_size=224,
-        max_length=128,
+        target_size=cfg.target_size,
+        max_length=cfg.temperature,
     )
     train_ds, val_ds = split_data(dataset, val_split=0.2)
     train_loader = DataLoader(
@@ -80,18 +87,17 @@ def main() -> None:
         weight_decay=cfg.weight_decay,
     )
     optim_wrapper = OptimWrapper(optimizer)
-    param_scheduler = MultiStepLR(optimizer, milestones=[8, 11], gamma=0.1)
+    param_scheduler = MultiStepLR(optimizer, milestones=[5, 8], gamma=0.1)
     runner = Runner(
         model=clip_model,
         train_dataloader=train_loader,
         optim_wrapper=optim_wrapper,
         param_scheduler=param_scheduler,
         train_cfg=dict(by_epoch=True, max_epochs=10, val_interval=1),
-        work_dir=
-        '/home/robin/work_dir/llm/MultimodalTransformers/work_dir/clip_model',
+        work_dir="/home/robin/work_dir/llm/MultimodalTransformers/work_dir/clip_model",
     )
     runner.train()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

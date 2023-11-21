47033c72f24415ce4498b3656feaca5e3f504eb1
jianzhnie <jianzhnie@126.com>
2023-11-21 19:40:44 +0800

    update train_clip

--- tools/train_clip.py
+++ tools/train_clip.py
@@ -1,13 +1,14 @@
 import sys
 
 import torch
-from mmengine.model import BaseModel
-from mmengine.optim import OptimWrapper
-from mmengine.runner import Runner
 from torch.utils.data import DataLoader, random_split
+
+from mmengine.model import BaseModel
+from mmengine.optim import AmpOptimWrapper
+from mmengine.runner import Runner
 from transformers import BertTokenizer
 
-sys.path.append('../')
+sys.path.append("../")
 from lmms.datasets.clip_dataset import Flikr8kDataset
 from lmms.models.clip.config import ClipConfig as cfg
 from lmms.models.clip.modeling_clip import CLIPModel
@@ -17,12 +18,12 @@ def split_data(dataset: Flikr8kDataset, val_split: float):
     train_length = int((1 - val_split) * len(dataset))
     val_length = len(dataset) - train_length
     train_dataset, val_dataset = random_split(
-        dataset, lengths=[train_length, val_length])
+        dataset, lengths=[train_length, val_length]
+    )
     return train_dataset, val_dataset
 
 
 class MMClipModel(BaseModel):
-
     def __init__(self, model):
         super().__init__()
         self.model = model
@@ -33,7 +34,7 @@ class MMClipModel(BaseModel):
         attention_mask,
         images,
         caption,
-        mode='loss',
+        mode="loss",
     ):
         outputs = self.model(
             input_ids,
@@ -41,8 +42,8 @@ class MMClipModel(BaseModel):
             images,
             caption,
         )
-        if mode == 'loss':
-            return {'loss': outputs[0]}
+        if mode == "loss":
+            return {"loss": outputs[0]}
 
 
 def main() -> None:
@@ -79,49 +80,57 @@ def main() -> None:
     ).to(cfg.device)
 
     clip_model = MMClipModel(model)
-    optimizer = torch.optim.AdamW(
-        model.parameters(),
-        lr=cfg.lr,
-        weight_decay=cfg.weight_decay,
+    optim_wrapper = dict(
+        type="OptimWrapper",
+        # 如果你想要使用 BF16，请取消下面一行的代码注释
+        # dtype='bfloat16',  # 可用值： ('float16', 'bfloat16', None)
+        optimizer=dict(
+            type="AdamW",
+            lr=cfg.lr,
+            weight_decay=cfg.weight_decay,
+        ),
+        # 累加 4 次参数更新一次
+        accumulative_counts=4
     )
-    optim_wrapper = OptimWrapper(optimizer)
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=cfg.lr, weight_decay=cfg.weight_decay
+    )
+
+    optim_wrapper = AmpOptimWrapper(optimizer=optimizer, dtype="float16")
+
     # learning policy
     warmup_epochs = 1  # about 10000 iterations for ImageNet-1k
     param_scheduler = [
         # warm up learning rate scheduler
         dict(
-            type='LinearLR',
+            type="LinearLR",
             start_factor=1e-3,
             by_epoch=True,
             end=warmup_epochs,
             convert_to_iter_based=True,
         ),
         # main learning rate scheduler
-        dict(type='CosineAnnealingLR',
-             eta_min=1e-5,
-             by_epoch=True,
-             begin=warmup_epochs),
+        dict(
+            type="CosineAnnealingLR", eta_min=1e-5, by_epoch=True, begin=warmup_epochs
+        ),
     ]
     # configure default hooks
     default_hooks = dict(
-        # record the time of every iteration.
-        timer=dict(type='IterTimerHook'),
-        # print log every 100 iterations.
-        logger=dict(type='LoggerHook', interval=100),
-        # enable the parameter scheduler.
-        param_scheduler=dict(type='ParamSchedulerHook'),
-        # save checkpoint per epoch.
-        checkpoint=dict(type='CheckpointHook', interval=1),
-        # set sampler seed in distributed evrionment.
-        sampler_seed=dict(type='DistSamplerSeedHook'),
+        logger=dict(type="LoggerHook", interval=10),
+    )
+    visualizer = dict(
+        type="Visualizer",
+        vis_backends=[
+            dict(type="WandbVisBackend", init_kwargs=dict(project="train_clip"))
+        ],
     )
     env_cfg = dict(
         # whether to enable cudnn benchmark
         cudnn_benchmark=False,
         # set multi process parameters
-        mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+        mp_cfg=dict(mp_start_method="fork", opencv_num_threads=0),
         # set distributed parameters
-        dist_cfg=dict(backend='nccl'),
+        dist_cfg=dict(backend="nccl"),
     )
     runner = Runner(
         model=clip_model,
@@ -131,12 +140,12 @@ def main() -> None:
         param_scheduler=param_scheduler,
         default_hooks=default_hooks,
         train_cfg=dict(by_epoch=True, max_epochs=10, val_interval=1),
-        default_scope='lmmtrain',
-        work_dir=
-        '/home/robin/work_dir/llm/MultimodalTransformers/work_dir/clip_model',
+        default_scope="lmmtrain",
+        visualizer=visualizer,
+        work_dir="/home/robin/work_dir/llm/MultimodalTransformers/work_dir/clip_model",
     )
     runner.train()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
